dda91aea6c75681d7a6734a96b23dfe3b72d08dd
Mitchell Stokes <mogurijin@gmail.com>
2014-04-14 19:18:51 -0700

    PreGame: Adding a layout to handle character creation.

--- src/scripts/player_data.py
+++ src/scripts/player_data.py
@@ -2,11 +2,12 @@ from scripts import spells
 
 
 class PlayerData:
-	def __init__(self, spell_list):
+	def __init__(self, name, spell_list):
+		self.name = name
 		self.spell_list = spell_list
 
 	@classmethod
-	def new(cls):
+	def new(cls, name):
 		# Create player data with four all damage spells
 		spell_list = []
 		sdna = spells.SpellDna()
@@ -14,4 +15,5 @@ class PlayerData:
 		for i in range(4):
 			spell_list.append(spells.Spell.from_dna(sdna))
 
-		return PlayerData(spell_list)
+		return PlayerData(name, spell_list)
+
--- src/scripts/pregame.py
+++ src/scripts/pregame.py
@@ -66,12 +66,67 @@ class Title(bgui_bge_utils.Layout):
 		self.menu.selected = self.menu.items[sidx]
 
 	def new_char(self):
-		logic.globalDict['player_data'] = PlayerData.new()
-		act = self.data.controller.actuators['StartGame']
-		self.data.controller.activate(act)
+		self.data.ui.load_layout(NewCharacter, self.data)
 
 	def exit(self):
 		logic.endGame()
+
+
+class NewCharacter(bgui_bge_utils.Layout):
+	def __init__(self, sys, data):
+		super().__init__(sys, data)
+
+
+		bgui.Label(self, text="New Student Registration", pt_size=48, pos=[0, 0.8], options=bgui.BGUI_CENTERX)
+		self.student_name = bgui.TextInput(self, prefix="Name: ", text="Enter a name", size=[0.8, 0.05], pos=[0, 0.6], options=bgui.BGUI_CENTERX | bgui.BGUI_INPUT_SELECT_ALL)
+		self.done_btn = bgui.Label(self, text="Done", pos=[0, 0.4], options=bgui.BGUI_CENTERX)
+
+		self.menu_options = OrderedDict([
+			(0, (self.student_name, self.name_enter, self.name_exit)),
+			(1, (self.done_btn, self.done_enter, None)),
+		])
+
+		self.selected = 0
+		self.entered = False
+
+		# Setup highlight
+		self.highlight = bgui.Frame(self, options=bgui.BGUI_NO_NORMALIZE)
+		self.highlight.colors = [[0.8, 0.8, 0.8, 0.8]] * 4
+
+	def update(self):
+		evts = logic.keyboard.events
+
+		if self.entered:
+			if evts[events.ENTERKEY] == logic.KX_INPUT_JUST_ACTIVATED:
+				self.entered = False
+				self.menu_options[self.selected][2]()
+		else:
+			if evts[events.ENTERKEY] == logic.KX_INPUT_JUST_ACTIVATED:
+				self.entered = True
+				self.menu_options[self.selected][1]()
+			if evts[events.DOWNARROWKEY] == logic.KX_INPUT_JUST_ACTIVATED:
+				self.selected = (self.selected + 1) % len(self.menu_options)
+			if evts[events.UPARROWKEY] == logic.KX_INPUT_JUST_ACTIVATED:
+				self.selected -= 1
+				if self.selected < 0:
+					self.selected = 0
+
+			highlight_widget = self.menu_options[self.selected][0]
+			self.highlight.size = highlight_widget.size
+			self.highlight.position = highlight_widget.position
+
+	def name_enter(self):
+		self.highlight.visible = False
+		self.student_name.activate()
+
+	def name_exit(self):
+		self.highlight.visible = True
+		self.student_name.deactivate()
+
+	def done_enter(self):
+		logic.globalDict['player_data'] = PlayerData.new(self.student_name.text)
+		act = self.data.controller.actuators['StartGame']
+		self.data.controller.activate(act)
 
 
 class PreGame:
